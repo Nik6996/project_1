ebcfce0ee7fc5124d15115dcbabc88a017bfeb7c
Nik6996 <nik.taraskov@ukr.net>
2021-01-11 07:36:32 +0400

    more

--- test_js/js/script.js
+++ test_js/js/script.js
@@ -15,26 +15,3 @@ $(document).ready(function () {
 
 
 
-// const status = 'ready'
-
-// if (status === 'ready') {
-// 	console.log('готово')
-// }
-// else if (status === 'pending') {
-// 	console.log('не готово')
-// }
-// else {
-// 	console.log('статус не найден')
-// }
-
-// let add = () => {
-// 	if (add == 100) {
-// 		console.log("cto");
-// 	}
-// 	else if (add === 20) {
-// 		console.log("rex")
-// 	}
-// }
-// add(100 - 80);
-
-
